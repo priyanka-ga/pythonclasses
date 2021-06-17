c45552890f958f6d118dfbf9c7b9aeee782d6e8e
Admin <Admin@Acer>
2021-06-17 10:06:14 +0530

    3rd program

---
+++ pythonclasses/src/pand_1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# a = [1, 7, 2]
+#
+# myvar = pd.Series(a)
+#
+# print(myvar)
+
+
+
+
+# a=[1,7,2]
+# s=pd.Series(a,index=["x","y","z"])
+# print(s)
+
+
+calories={"day1":420,"day2":380,"day3":390}
+s=pd.Series(calories)
+print(s)
